b2fecc75452a27e3017149dc058831e33fc8efe5
Camille <83260908+Camille846@users.noreply.github.com>
2023-01-03 23:35:02 -0300

    Feat: Conditions for Enter and Delete Key Values

--- src/components/Key.jsx
+++ src/components/Key.jsx
@@ -4,12 +4,27 @@ import { AppContext } from '../App'
 const Key = ( {keyVal, bigKey} ) => {
   const { board, setBoard, currentAttempt, setCurrentAttempt } = useContext(AppContext)
   const selectLetter = () => {
-    // Return if the current attempt is complete
-    if(currentAttempt.letterPosition >= board[currentAttempt.attempt].length) return
-    const newBoard = [...board]
-    newBoard[currentAttempt.attempt][currentAttempt.letterPosition] = keyVal
-    setBoard(newBoard)
-    setCurrentAttempt({...currentAttempt, letterPosition: currentAttempt.letterPosition + 1})
+    if(keyVal === 'ENTER') {
+      // Return if the current attempt is not complete
+      if(currentAttempt.letterPosition !== 5) return
+      setCurrentAttempt({...currentAttempt, attempt: currentAttempt.attempt + 1, letterPosition: 0})
+
+    } else if(keyVal === 'DELETE') {
+      // Return if the current attempt is complete
+      if(currentAttempt.letterPosition === 0) return
+      const newBoard = [...board]
+      newBoard[currentAttempt.attempt][currentAttempt.letterPosition - 1] = ''
+      setBoard(newBoard)
+      setCurrentAttempt({...currentAttempt, letterPosition: currentAttempt.letterPosition - 1})
+    } else {
+      // Return if the current attempt is complete
+      if(currentAttempt.letterPosition >= board[currentAttempt.attempt].length) return
+  
+      const newBoard = [...board]
+      newBoard[currentAttempt.attempt][currentAttempt.letterPosition] = keyVal
+      setBoard(newBoard)
+      setCurrentAttempt({...currentAttempt, letterPosition: currentAttempt.letterPosition + 1})
+    }
   }
 
   return (
