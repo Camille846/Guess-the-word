beba745459fb336e541fe8eab0016b689fce7313
Camille <83260908+Camille846@users.noreply.github.com>
2023-01-04 17:54:37 -0300

    Feat: Create a function to handle keyboard events

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,14 +1,51 @@
-import React from 'react'
+import React, { useContext, useCallback, useEffect } from 'react'
+import { AppContext } from '../App'
 import Key from './Key'
 import { FiDelete } from 'react-icons/fi'
 
 const Keyboard = () => {
+  const { onSelectedEnter, onSelectedDelete, onSelectedLetter } = useContext(AppContext)
   const keys1 = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P']
   const keys2 = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L']
   const keys3 = ['Z', 'X', 'C', 'V', 'B', 'N', 'M']
 
+// Create a function to handle keyboard events
+  const handleKeyboard = useCallback ((e) => {
+    if(e.key === 'Enter') {
+      onSelectedEnter()
+    } else if(e.key === 'Backspace') {
+      onSelectedDelete()
+    } else{
+      keys1.forEach((key) => { 
+        if(e.key.toUpperCase() === key) {
+          onSelectedLetter(key)
+        }
+      })
+      keys2.forEach((key) => {
+        if(e.key.toUpperCase() === key) {
+          onSelectedLetter(key)
+        }
+      })
+      keys3.forEach((key) => {
+        if(e.key.toUpperCase() === key) {
+          onSelectedLetter(key)
+        }
+      })
+    }
+
+  })
+
+// using useEffect to handle keyboard events 
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyboard)
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyboard)
+    }
+  }, [handleKeyboard])
+
   return (
-    <div className='keyboard'>
+    <div className='keyboard' onKeyDown={handleKeyboard}>
       <div className="first_line">
         {keys1.map((key) => {
           return <Key keyVal={key}/>
