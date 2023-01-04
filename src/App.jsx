5ebd47b1c5ac372efb0f5bbbc77de66bd558b66e
Camille <83260908+Camille846@users.noreply.github.com>
2023-01-04 17:40:11 -0300

    Feat:  functions for selected keys

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,37 @@ export const AppContext = createContext()
 function App() {
   const [board, setBoard] = useState(boardDefault)
   const [currentAttempt, setCurrentAttempt] = useState({attempt: 0, letterPosition: 0})
+
+  const onSelectedLetter = (keyVal) => {
+    if(currentAttempt.letterPosition >= board[currentAttempt.attempt].length) return
+  
+    const newBoard = [...board]
+    newBoard[currentAttempt.attempt][currentAttempt.letterPosition] = keyVal
+    setBoard(newBoard)
+    setCurrentAttempt({...currentAttempt, letterPosition: currentAttempt.letterPosition + 1})
+  }
+  
+  const onSelectedEnter = () => {
+    if(currentAttempt.letterPosition !== 5) return
+    setCurrentAttempt({...currentAttempt, attempt: currentAttempt.attempt + 1, letterPosition: 0})
+  }
+
+  const onSelectedDelete = () => {
+    if(currentAttempt.letterPosition === 0) return
+    const newBoard = [...board]
+    newBoard[currentAttempt.attempt][currentAttempt.letterPosition - 1] = ''
+    setBoard(newBoard)
+    setCurrentAttempt({...currentAttempt, letterPosition: currentAttempt.letterPosition - 1})
+  }
+
   return (
     <div className="App">
       <nav>
         <h1>Guess the word!</h1>
       </nav>
-      <AppContext.Provider value={{ board, setBoard, currentAttempt, setCurrentAttempt }}>
+      <AppContext.Provider 
+        value={{ board, setBoard, currentAttempt, setCurrentAttempt, onSelectedLetter, onSelectedEnter, onSelectedDelete }}
+      >
         <Board />
         <Keyboard />
       </AppContext.Provider>
--- src/components/Key.jsx
+++ src/components/Key.jsx
@@ -2,28 +2,18 @@ import React, { useContext } from 'react'
 import { AppContext } from '../App'
 
 const Key = ( {keyVal, bigKey} ) => {
-  const { board, setBoard, currentAttempt, setCurrentAttempt } = useContext(AppContext)
+  const { 
+    onSelectedLetter, 
+    onSelectedEnter, 
+    onSelectedDelete 
+  } = useContext(AppContext)
   const selectLetter = () => {
     if(keyVal === 'ENTER') {
-      // Return if the current attempt is not complete
-      if(currentAttempt.letterPosition !== 5) return
-      setCurrentAttempt({...currentAttempt, attempt: currentAttempt.attempt + 1, letterPosition: 0})
-
+      onSelectedEnter()
     } else if(keyVal === 'DELETE') {
-      // Return if the current attempt is complete
-      if(currentAttempt.letterPosition === 0) return
-      const newBoard = [...board]
-      newBoard[currentAttempt.attempt][currentAttempt.letterPosition - 1] = ''
-      setBoard(newBoard)
-      setCurrentAttempt({...currentAttempt, letterPosition: currentAttempt.letterPosition - 1})
+      onSelectedDelete()
     } else {
-      // Return if the current attempt is complete
-      if(currentAttempt.letterPosition >= board[currentAttempt.attempt].length) return
-  
-      const newBoard = [...board]
-      newBoard[currentAttempt.attempt][currentAttempt.letterPosition] = keyVal
-      setBoard(newBoard)
-      setCurrentAttempt({...currentAttempt, letterPosition: currentAttempt.letterPosition + 1})
+      onSelectedLetter(keyVal)
     }
   }
 
