685b4ac9f688abbcb723a649d57fc3709b2b7088
Camille <83260908+Camille846@users.noreply.github.com>
2023-01-03 23:27:42 -0300

    Feat: Condition to return if the current attempt is complete

--- src/components/Key.jsx
+++ src/components/Key.jsx
@@ -4,6 +4,8 @@ import { AppContext } from '../App'
 const Key = ( {keyVal, bigKey} ) => {
   const { board, setBoard, currentAttempt, setCurrentAttempt } = useContext(AppContext)
   const selectLetter = () => {
+    // Return if the current attempt is complete
+    if(currentAttempt.letterPosition >= board[currentAttempt.attempt].length) return
     const newBoard = [...board]
     newBoard[currentAttempt.attempt][currentAttempt.letterPosition] = keyVal
     setBoard(newBoard)
