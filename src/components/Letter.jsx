d23fb10c8761df7e565fcb6452a5733232acc557
Camille <83260908+Camille846@users.noreply.github.com>
2023-01-05 23:20:49 -0300

    Feat: toUpperCase method for words

--- src/components/Letter.jsx
+++ src/components/Letter.jsx
@@ -5,8 +5,8 @@ import { AppContext } from '../App'
 const Letter = ({letterPos, attemptVal}) => {
     const { board, correctWord, currentAttempt, setDisabledLetters } = useContext(AppContext)
     const letter = board[attemptVal][letterPos]
-    const correct = correctWord[letterPos] === letter
-    const almost = !correct && letter !== ' ' && correctWord.includes(letter)
+    const correct = correctWord.toUpperCase()[letterPos] === letter
+    const almost = !correct && letter !== ' ' && correctWord.toUpperCase().includes(letter)
     // we just want to set the colors after the user has made an attempt
     const letterState = currentAttempt.attempt > attemptVal && (correct ? 'correct' : almost ? 'almost' : 'error')
 
