76944ba2dd73929df90a1cde2b2b90a61dd59455
Camille <83260908+Camille846@users.noreply.github.com>
2023-01-05 23:20:06 -0300

    Feat: create array for wordle bank words

--- src/components/Words.js
+++ src/components/Words.js
@@ -11,11 +11,13 @@ export const boardDefault = [
 
 export const generateWordSet = async () => {
     let wordSet;
+    let todaysWord;
     await fetch(wordleBank)
         .then((response) => response.text()) 
         .then((result) => {
             const wordArray = result.split('\n')
+            todaysWord = wordArray[Math.floor(Math.random() * wordArray.length)];
             wordSet = new Set(wordArray);
         });
-    return {wordSet};
+    return {wordSet, todaysWord};
 }; 
